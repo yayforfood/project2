d21eea652bd20b76a45f84558e12f4fcc7645846
sweinstein <raeweinst@gmail.com>
2019-12-16 10:19:07 -0500

    api call test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+	async componentDidMount() {
+		console.log(await (axios.get("http://www.omdbapi.com/?i=tt0092455&apikey=2e6c17f5&season=1")))
+	}
+	render() {
+		return (
+			<div className="App">
+
+			</div>
+		);
+	}
 }
 
 export default App;
