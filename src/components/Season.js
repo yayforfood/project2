c247e9ae952e4778188d9b6af7044659edaa174e
sweinstein <raeweinst@gmail.com>
2019-12-19 21:51:28 -0500

    write code for API_season

--- src/components/Season.js
+++ src/components/Season.js
@@ -3,7 +3,9 @@ import { API_season } from '../services/api-helper';
 export default class Season extends Component {
     constructor(props) {
         super(props);
-
+        this.state{
+            
+        }
     }
     render() {
         return (
--- src/services/api-helper.js
+++ src/services/api-helper.js
@@ -9,6 +9,6 @@ export const API_id = async (i) => {
     const response = await axios.get(`http://www.omdbapi.com/?apikey=${key}&i=${i}&plot=full`);
     return response.data;
 }
-export const API_season = async (show) => {
-    
+export const API_season = async (i, season) => {
+    const response = await axios.get(`http://www.omdbapi.com/?apikey=${key}&i=${i}&season=${season}`)
 }
